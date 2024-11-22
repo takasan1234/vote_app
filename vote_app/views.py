85f57545671770cc3d66ee81cfad8064180d5396
takaya <takaya_san@icloud.com>
2024-11-22 19:47:22 +0900

    first commit

---
+++ vote_app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
+
+class Vote(TemplateView):
+    # 投票ページを表示
+    def get(self, request):
+        return render(request, 'vote_page.html')
+    # 投票ページで投票した選択肢をセッションに保存
+    def post(self, request):
+        vote = request.POST.get('vote') # 投票した選択肢を取得　例: 'A'
+        request.session['vote'] = vote  # セッションに投票した選択肢を保存
+        return redirect('result')
+
+class Result(TemplateView):
+    # セッションから投票した選択肢を取得して結果ページに表示
+    def get(self, request):
+        vote = request.session.get('vote')
+        return render(request, 'result_page.html', {'vote': vote})
